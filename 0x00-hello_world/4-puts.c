44d355a4cf5ef54a7d81ddf2cbffcd8964683d07
Randiper <consultozonkwo@gmail.com>
2022-09-16 03:34:27 -0700

    program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * About - prints out a string to stdout
+ * return 0; (if correct)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
